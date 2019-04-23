136adb8cc13bc335286666b104c1b5075249bc13
Thiago Prestes <thiagohenriqueprestes@gmail.com>
2019-04-23 12:07:01 -0300

    Alterações na auditoria

--- src/DEKL.CP.Domain/Entities/Audit.cs
+++ src/DEKL.CP.Domain/Entities/Audit.cs
@@ -8,6 +8,7 @@ namespace DEKL.CP.Domain.Entities
         public ApplicationUser ApplicationUser { get; set; }
         public int ModuleId { get; set; }
         public Module Module { get; set; }
+        public int UpdatedRecordId { get; set; }
         public string Event { get; set; }
         public DateTime DateTime { get; set; }
     }
